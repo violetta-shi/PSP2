e490afe63d9917d2132df2e2191539081cd98cdf
violetta-shi <violetta.shishonok@gmail.com>
2023-09-26 15:19:01 +0300

    create class Mechanic

--- src/service/employee/Human.java
+++ src/service/employee/Human.java
@@ -4,4 +4,11 @@ public abstract class Human {
     private String FIO;
     private String ID;
 
+    public String getFIO() {
+        return FIO;
+    }
+
+    public String getID() {
+        return ID;
+    }
 }
---
+++ src/service/employee/Mechanic.java
@@ -0,0 +1,5 @@
+package service.employee;
+
+public class Mechanic extends Human{
+
+}
